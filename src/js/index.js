063bfeef9f50684aad8dc22aa5a4c20cc7d89d2b
Valentin Drany <navshop@ukr.net>
2016-09-14 12:26:01 +0300

    fix animation text

--- src/js/index.js
+++ src/js/index.js
@@ -66,7 +66,7 @@
 //Fuction 'dx' move and opacity letter
 	function dx()
 	{
-		if ( dxFactor == 1500 ) return
+		if ( opacityFactor < 0 ) return
 		
 		$texts[$texts.length-1].setAttribute("dx", dxFactor++)
 		$texts[$texts.length-1].setAttribute("fill-opacity", opacityFactor -= 0.05)
@@ -84,6 +84,9 @@
 		$texts[$texts.length-7].setAttribute("fill-opacity", opacityFactor -= 0.001)
 		$texts[$texts.length-8].setAttribute("dx", dxFactor++)
 		$texts[$texts.length-8].setAttribute("fill-opacity", opacityFactor -= 0.001)
+		$texts[$texts.length-9].setAttribute("dx", dxFactor++)
+		$texts[$texts.length-9].setAttribute("fill-opacity", opacityFactor -= 0.001)
+
 
 		setTimeout(dx, 100)
 	}
